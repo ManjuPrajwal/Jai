9df445851a80e25836de8c7280278648f453e6d4
ManjuPrajwal <umanju4@gmail.com>
2019-09-28 00:50:56 +0800

    initial commit

---
+++ Javascript/Common.js
@@ -0,0 +1,28 @@
+﻿// Allow only Numbers
+
+function checkNumber(evt) {
+    evt = (evt) ? evt : window.event
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        status = "This field accepts numbers only."
+        return false
+    }
+    status = ""
+    return true
+}
+
+
+//Allow only Characters
+
+function checkCharacters(evt) {
+    evt = (evt) ? evt : window.event
+    var charCode = (evt.which) ? evt.which : evt.keyCode
+    if ((charCode >= 97 && charCode <= 122) || (charCode >= 65 && charCode <= 90) || (charCode == 32 ||
+charCode == 46)) {
+        return true;
+    } else {
+        alert("Allow only characters");
+        return false;
+    }
+}
+
